dc00c80648798713fe395ff81e500b2cf991c8d6
fomafee <onam3125@gmail.com>
2016-10-20 11:33:19 +0900

    ㅁㄴㄹㄴㅁㅇ

--- test2/src/main/java/com/rhs/command/SeeThis.java
+++ test2/src/main/java/com/rhs/command/SeeThis.java
@@ -1,19 +1,20 @@
 package com.rhs.command;
 
-<<<<<<< HEAD
-=======
+
 /**
  * commit test
  * @author nssong
  * FUCKING
  */
->>>>>>> origin/master
+
 public class SeeThis {
 	
 	public void main(String args[])
 	{
 		System.out.println("BURNING KOSEA");
 		System.out.println("yoon");
+		
+		//수정합니다
 	}
 
 }
